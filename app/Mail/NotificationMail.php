04b203ba2dbdec960ad659f59c744452bcf73b0f
AJ Aparicio <aparicio.anthonyjohn@gmail.com>
2026-01-15 12:55:19 +0800

    feat: add mail service

---
+++ app/Mail/NotificationMail.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\Notification;
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class NotificationMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public function __construct(
+        public string $subject,
+        public string $notificationMessage,
+        public string $type,
+        public User $user
+    ) {}
+
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: '[Relief] ' . $this->subject,
+        );
+    }
+
+    public function content(): Content
+    {
+        return new Content(
+            view: 'emails.notification',
+            with: [
+                'notificationMessage' => $this->notificationMessage,
+                'type' => $this->type,
+                'color' => Notification::getColor($this->type),
+                'userName' => $this->user->name,
+            ],
+        );
+    }
+}
